d13d4f1d3161e3d46d76be00af206c3f144a175d
Daniel Zárate <49570493+Danifado@users.noreply.github.com>
2021-03-19 21:42:10 -0500

    Actualizacion

--- interpolacionCuadratica.m
+++ interpolacionCuadratica.m
@@ -3,72 +3,96 @@
 %Miguel Caicedo
 %Implementacion de Seccion Aurea
 
-%paso 0
+%Paso 0
 iter = 0;
 x1 = -3;
 x2 = -1;
 x3 = 5;
-int_i = [x1,x2,x3];  % intervalo inicial
-
-fx_1= x1^2-2*x1;    % f(x1)
-fx_2= x2^2-2*x2;    % f(x2)
-fx_3= x3^2-2*x3;    % f(x3)
+intervalo = [x1,x2,x3];  % intervalo inicial
 
 
+%(2 * exp(-x)) + (2 * x^2)
 
 l = 0.2;             % epsilon 
  
-%paso 1
+f1 = f(intervalo(1));  % f(x1)
+f2 = f(intervalo(2));  % f(x2)
+f3 = f(intervalo(3));  % f(x3)
+
+a23 = intervalo(2) - intervalo(3);
+a31 = intervalo(3) - intervalo(1);
+a12 = intervalo(1) - intervalo(2);
+
+b23 = intervalo(2) - intervalo(3);
+b31 = intervalo(3) - intervalo(1);
+b12 = intervalo(1) - intervalo(2);
+
+xg = (b23 * f1) + (b31 * f2) + (b12 * f3); % x_gorro
+xg = xg / (2 * ((a23 * f1) + (a31 * f2) + (a12 * f3)));
+
+%Paso 1
 while true
+intervalo
+disp("==========================")
+f1 = f(intervalo(1));  % f(x1)
+f2 = f(intervalo(2));  % f(x2)
+f3 = f(intervalo(3));  % f(x3)
 
-xg = (((x2^2-x3^2)*fx_1)+((x3^2-x1^2)*fx_2)+((x1^2-x2^2)*fx_3))/2*(((x2-x3)*fx_1)+((x3-x1)*fx_2)+((x1-x2)*fx_3)); % x gorro
-fx_g = xg^2-2*xg;    % f(x_gorro)         
+a23 = intervalo(2) - intervalo(3);
+a31 = intervalo(3) - intervalo(1);
+a12 = intervalo(1) - intervalo(2);
 
-iter=iter+1;
-if abs(int_i(1,3)- int_i(1,1)) < l
+b23 = intervalo(2) - intervalo(3);
+b31 = intervalo(3) - intervalo(1);
+b12 = intervalo(1) - intervalo(2);
+
+fxg = f(xg);    % f(x_gorro)
+
+iter = iter + 1;
+
+if abs(intervalo(3)- intervalo(1)) < l
     break
 else
-    if xg > x2
-        %Paso 2 
-        if fx_g >= fx_2
-            int_i=[x1,x2,xg];
-            
-        elseif fx_g <= fx_2 
-            int_i = [x2,xg,x3];    
-            end
-     elseif xg < x2  
-        % paso 3
-        if fx_g >= fx_2
-            int_i=[xg,x2,x3];
-            
-        elseif fx_g <= fx_2 
-            int_i = [x1,xg,x2]; 
-            end
-     elseif xg == x2
-        % paso 4
-        if x2-x1 < x3-x2
-            x2 + (l/2);
-            
-        elseif x2-x1 > x3-x2
-            x2-(l/2); 
-            end
-        
-    end     
+    if xg > intervalo(2)
+        disp("Paso 2")
+        %Paso 2
+        if fxg > f2
+            intervalo = [x1,x2,xg];
+
+        else
+            intervalo = [x2,xg,x3];
+        end
+     elseif xg < intervalo(2)
+        disp("Paso 3")
+        % Paso 3
+        if fxg > f2
+            intervalo=[xg,x2,x3];
+
+        else
+            intervalo = [x1,xg,x2];
+        end
+     elseif xg == intervalo(2)
+        disp("Paso 4")
+        % Paso 4
+        disp("=============================")
+        if x2 - x1 < x3 - x2
+            xg = intervalo(2) + (l / 2);
+    
+        elseif x2 - x1 > x3 - x2
+            xg = intervalo(2) - (l / 2);
+        end
+    else
+        break
+    end
 end
 
 disp("Iteracion numero: ")
 disp(iter)
 disp("Los valores de las variables son: ")
-fx_1
-fx_2
-fx_3
+intervalo
 
-xg
-fx_g
-
-int_i
-%wait time (5 second);
-%break
 end
 
-
+function y = f(x)
+    y = (2 * exp(-x)) + (2 * x^2);
+end
